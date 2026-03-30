b66c81de4017c430aff21e5f0041cb1e41d70da9
Leeonealves <125828959+Leeonealves@users.noreply.github.com>
2026-03-30 01:21:06 +0100

    implement i18next browser language detector

--- Frontend/i18n/index.ts
+++ Frontend/i18n/index.ts
@@ -1,21 +1,28 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 import pt from "./locales/pt/common.json";
 import en from "./locales/en/common.json";
 import es from "./locales/es/common.json";
 
-i18n.use(initReactI18next).init({
-  resources: {
-    pt: { translation: pt },
-    en: { translation: en },
-    es: { translation: es },
-  },
-  lng: localStorage.getItem("language") || "pt",
-  fallbackLng: "pt",
-  interpolation: {
-    escapeValue: false,
-  },
-});
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      pt: { translation: pt },
+      en: { translation: en },
+      es: { translation: es },
+    },
+    fallbackLng: "pt",
+    interpolation: {
+      escapeValue: false,
+    },
+    detection: {
+      order: ["localStorage", "navigator", "htmlTag"],
+      caches: ["localStorage"],
+    },
+  });
 
 export default i18n;
